494e1ac24e28e06fc18fe2e605828507cdf3891d
wangjiuchun <17694902569@163.com>
2022-04-28 16:55:27 +0800

    加入vue-grid-layout

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+const app = createApp(App)
 
 import './styles/base.css'
 
 // router
 import router from "@/router"
+app.use(router)
 
 // element-plus
-import elementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import elementPlus from "element-plus"
+import "element-plus/dist/index.css"
+app.use(elementPlus)
 
-createApp(App).use(router).use(elementPlus).mount('#app')
+import VueGridLayout from "vue-grid-layout"
+app.use(VueGridLayout)
+
+app.mount('#app')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,29 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import HelloWorldVue from "../../components/HelloWorld.vue";
 
 // 响应式状态
-const count = ref(0)
+const layout = ref([
+  { "x": 0, "y": 0, "w": 2, "h": 2, "i": "0" },
+  { "x": 2, "y": 0, "w": 2, "h": 4, "i": "1" },
+  { "x": 4, "y": 0, "w": 2, "h": 5, "i": "2" },
+  { "x": 6, "y": 0, "w": 2, "h": 3, "i": "3" },
+  { "x": 8, "y": 0, "w": 2, "h": 3, "i": "4" },
+  { "x": 10, "y": 0, "w": 2, "h": 3, "i": "5" },
+  { "x": 0, "y": 5, "w": 2, "h": 5, "i": "6" },
+  { "x": 2, "y": 5, "w": 2, "h": 5, "i": "7" },
+  { "x": 4, "y": 5, "w": 2, "h": 5, "i": "8" },
+  { "x": 6, "y": 3, "w": 2, "h": 4, "i": "9" },
+  { "x": 8, "y": 4, "w": 2, "h": 4, "i": "10" },
+  { "x": 10, "y": 4, "w": 2, "h": 4, "i": "11" },
+  { "x": 0, "y": 10, "w": 2, "h": 5, "i": "12" },
+  { "x": 2, "y": 10, "w": 2, "h": 5, "i": "13" },
+  { "x": 4, "y": 8, "w": 2, "h": 4, "i": "14" },
+  { "x": 6, "y": 8, "w": 2, "h": 4, "i": "15" },
+  { "x": 8, "y": 10, "w": 2, "h": 5, "i": "16" },
+  { "x": 10, "y": 4, "w": 2, "h": 2, "i": "17" },
+  { "x": 0, "y": 9, "w": 2, "h": 3, "i": "18" },
+])
 
 // 生命周期钩子
 onMounted(() => {
@@ -11,6 +32,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="home-wrap h-full bg-blue-100"></div>
+  <div class="home-wrap h-full bg-blue-100">
+    <grid-layout v-model:layout="layout" :col-num="12" :row-height="30" :is-draggable="true" :is-resizable="true"
+      :is-mirrored="false" :vertical-compact="true" :margin="[10, 10]" :use-css-transforms="true">
+      <grid-item v-for="item in layout" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i" :key="item.i"
+        class="border border-gray-900 overflow-hidden"
+      >
+        <hello-world-vue></hello-world-vue>
+      </grid-item>
+    </grid-layout>
+  </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
